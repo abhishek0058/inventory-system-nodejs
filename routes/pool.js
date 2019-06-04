0b445ec98a667b3766b248bc8d1fec7326f3be28
Abhishek Sharma <abhishek0058@gmail.com>
2019-06-04 22:51:26 +0530

    password change

--- routes/pool.js
+++ routes/pool.js
@@ -3,7 +3,7 @@ var mysql = require('mysql')
 const pool = mysql.createPool({
     host: '142.93.211.148',
     user: 'root',
-    password: '123',
+    password: 'Abhishek@0058',
     database: 'inventory',
     multipleStatements: true,
     connectionLimit: 100
