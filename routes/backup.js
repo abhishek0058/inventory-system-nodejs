abee2a2fa31657de7b710d4885478c269a7cb079
abhishek sharma <abhishek0058@gmail.com>
2019-06-10 09:52:34 +0000

    implemented scripts

--- routes/backup.js
+++ routes/backup.js
@@ -3,20 +3,11 @@ const router = require('express').Router();
 var schedule = require('node-schedule');
 var exec = require('child_process').exec;
 
-const shellScript = `mysqldump -u root -p${process.env.DB_PASSWORD} ${process.env.DB_PROJECT} > db_backup.sql`;
+const shellScript = `mysqldump -u root -p${process.env.DB_PASSWORD} ${process.env.DB_PROJECT} > public/db_backup.sql`;
 
-var j = schedule.scheduleJob({ second: 0 }, function(){
-    var child = exec(shellScript);
+var j = schedule.scheduleJob({ minute: 0, hour: 0 }, function(){
+	console.log("executing script");
+	var child = exec(shellScript);
 });
-
-
-// router.get('/get-back-up', (req, res) => {
-//     if (req.session.adminId) {
-//         res.render('admin/home', req.session);
-//     } else {
-//         res.json({ error: true, message: "Please log-in" });
-//     }
-// });
-
 
 module.exports = router;
